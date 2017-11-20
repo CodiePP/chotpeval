da1224c0aa3c2f1f9ca6b31de441974f0725b862
Alexander Diemand <codieplusplus@apax.net>
2017-11-20 16:11:12 +0100

    only keeping validated random number generator

--- src/HCOTP/Computation/Random.hs
+++ src/HCOTP/Computation/Random.hs
@@ -2,8 +2,7 @@
 
 module HCOTP.Computation.Random
        (
-         get_random
-       , get_random_k
+         get_random_k
        )
 where
 
@@ -13,9 +12,11 @@ import System.Random
 -- | get a random number in the interval (0,1] (by specification)
 --
 --   here: lower bound is 1.0e-32
+{-
 get_random :: IO Double
 get_random =
   getStdRandom (randomR (0.0, 1.0)) >>= \r -> return $ max 1.0e-32 r
+-}
 
 -- | integer version of random number in the range [1, 1000]
 {-@  get_random_k :: IO {v : Nat | v >= 1 && v <= 1000 } @-}
